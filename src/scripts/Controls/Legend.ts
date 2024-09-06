a18f166c4a1cab5b3ac01bd976d5d1985ac1330a
Kevin Carmody <kevin@skinofstars.com>
2024-09-06 22:12:46 +0100

    feature: add visibility checkboxes to legend

--- src/scripts/Controls/Legend.ts
+++ src/scripts/Controls/Legend.ts
@@ -1,36 +1,70 @@
-import * as L from 'leaflet';
+import { EventTopics } from '../EventTopics';
 import { IMapLayer } from '../layers/IMapLayer';
+import * as L from 'leaflet';
+import PubSub from 'pubsub-js';
 
 export class Legend {
     static create = (layers: Map<string, IMapLayer>, activeLayers: Array<string>) => {
-        const legend = new L.Control({ position: "topright" });
+        const legend = new L.Control({ position: 'topright' });
 
         const div = document.createElement('div');
         div.classList.add('legend');
 
-        const header = document.createElement('h4');
-        header.textContent = 'Legend';
-
-        div.appendChild(header);
+        // const header = document.createElement('h4');
+        // header.textContent = 'Legend';
+        // div.appendChild(header);
 
         const ul = document.createElement('ul');
 
         layers.forEach((layer: IMapLayer, layerName) => {
             if (activeLayers.includes(layerName)) {
-                ul.appendChild(layer.getLegendEntry());
+                // we want to display a checkbox beside each item.
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.id = `${layer.id}-legend-checkbox`;
+
+                // for first load, use the visible property to determine if it is checked
+                checkbox.checked = layer.visible;
+
+                // bind for checkbox action
+                checkbox.addEventListener('change', (e) => {
+                    checkbox.checked
+                        ? PubSub.publish(EventTopics.showLayer, layer.id)
+                        : PubSub.publish(EventTopics.hideLayer, layer.id);
+                });
+
+                // create container div for checkbox and legend entry HTML component
+                const div = document.createElement('div');
+                div.classList.add('flex', 'pb-2');
+
+                let legendEntry = layer.getLegendEntry();
+                legendEntry.classList.add('flex-1', 'pr-2');
+                div.appendChild(legendEntry);
+
+                checkbox.classList.add('h-4', 'w-4', 'rounded', 'border-gray-300');
+                div.appendChild(checkbox);
+
+                ul.appendChild(div);
             }
         });
 
-        div.appendChild(ul);
+        // bind for checkbox visibility
+        PubSub.subscribe(EventTopics.showLayer, (msg, layerId) => {
+            const box = document.getElementById(`${layerId}-legend-checkbox`) as HTMLInputElement;
+            if (box) box.checked = true;
+        });
 
-        const instructions = document.createElement('div');
-        instructions.textContent = 'Click item to toggle visibility';
-        div.appendChild(instructions);
+        PubSub.subscribe(EventTopics.hideLayer, (msg, layerId) => {
+            const box = document.getElementById(`${layerId}-legend-checkbox`) as HTMLInputElement;
+            if (box) box.checked = false;
+        });
+
+        div.appendChild(ul);
 
         legend.onAdd = (map) => {
             return div;
         };
 
         return legend;
-    }
+    };
 }
